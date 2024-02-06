8d0c964ea2377ad31dcbc1de96c9f753bae43c35
Kaporos <theo@daron.be>
2024-02-06 18:45:17 +0100

    split status patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
+static const int splitstatus        = 1;        /* 1 for split status items */
+static const char *splitdelim        = ";";       /* Character used for separating status */
 static const char dmenufont[]       = "monospace:size=10";
 static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
--- config.h
+++ config.h
@@ -6,13 +6,15 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=7" };
+static const int splitstatus        = 1;        /* 1 for split status items */
+static const char *splitdelim        = ";";       /* Character used for separating status */
 static const char dmenufont[]       = "monospace:size=7";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static unsigned int baralpha        = 0xd0;
+static unsigned int baralpha        = 0xC8;
 static unsigned int borderalpha     = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
